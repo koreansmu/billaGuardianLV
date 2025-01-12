744f2e45b0f4ea5767dc0ce2697902a033f61544
koreansmu <shuklakusum4q@gmail.com>
2025-01-12 11:16:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ OWNER_ID = 6346273488
 
 SUDO_ID = [5907205317, 1805959544, 1284920298, 5881613383, 6864672519, 5458968679]
 
-MONGO_URI = ""
+MONGO_URI = "mongodb+srv://aditya20278:#raam7879636352@editguardian.vablt.mongodb.net/?retryWrites=true&w=majority&appName=editguardian"
 
 DB_NAME = "interstellarXd"
 
