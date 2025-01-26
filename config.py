85a8dbe8c57d453d156c3ad1ca8d45a77b9e0cb3
koreansmu <shuklakusum4q@gmail.com>
2025-01-26 12:13:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 API_ID = 24845544
 
-API_HASH = "57f32b90d5406552ab9156f6404fe5a8"
+API_HASH = "
 
 # Telegram bot token
-TELEGRAM_TOKEN = "7668941093:AAGSH1aycwqr0NbFR1YHZUW0Yyr66MChAR4"
+TELEGRAM_TOKEN = "
 
 # Your telegram user ID (owner)
 OWNER_ID = 5960968099
 
 SUDO_ID = [6257927828]
 
-MONGO_URI = "mongodb+srv://aditya20278:#raam7879636352@editguardian.vablt.mongodb.net/?retryWrites=true&w=majority&appName=editguardian"
+MONGO_URI = "
 
-DB_NAME = "aditya20278"
+DB_NAME = "
 
 LOGGER = True
 
