eacc7d32039886f24a92be0488184a369d193453
koreansmu <shuklakusum4q@gmail.com>
2025-02-06 23:57:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -671,6 +671,8 @@ def main():
     dispatcher.add_handler(CommandHandler("broadcast", broadcast_command))
     dispatcher.add_handler(CommandHandler("replybroadcast", reply_broadcast_command))
 
+dispatcher.add_handler(CommandHandler("getid", get_id))
+
     # Start the Bot
     updater.start_polling()
     updater.idle()
