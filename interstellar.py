c1ecada8a32a236493ab448c63a118362c5cc123
koreansmu <shuklakusum4q@gmail.com>
2025-03-10 02:33:11 +0530

    Update interstellar.py

--- interstellar.py
+++ interstellar.py
@@ -1,48 +1,49 @@
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
 PM_START_TEXT = """
-*Hᴇʟʟᴏ* {}[💎]({})🖖 
-"I'ᴍ ʏᴏᴜʀ Bɪʟʟᴀ Eᴅɪᴛ Gᴜᴀʀᴅɪᴀɴ Bᴏᴛ"
-ʜᴇʀᴇ ᴛᴏ ᴍᴀɪɴᴛᴀɪɴ ᴀ sᴇᴄᴜʀᴇ ᴇɴᴠɪʀᴏɴᴍᴇɴᴛ ғᴏʀ ᴏᴜʀ ɢʀᴏᴜᴘ ᴅɪsᴄᴜssɪᴏɴs.
+*Hᴇʟʟᴏ* {}[💎]({})🖖  
+"I'ᴍ ʏᴏᴜʀ Bɪʟʟᴀ Eᴅɪᴛ Gᴜᴀʀᴅɪᴀɴ Bᴏᴛ", ʜᴇʀᴇ ᴛᴏ sᴀғᴇɢᴜᴀʀᴅ ᴏᴜʀ ɢʀᴏᴜᴘs!
 
-🚫 ᴇᴅɪᴛᴇᴅ ᴍᴇꜱꜱᴀɢᴇ ᴅᴇʟᴇᴛɪᴏɴ : ɪ"ʟʟ ʀᴇᴍᴏᴠᴇ ᴇᴅɪᴛᴇᴅ ᴍᴇꜱꜱᴀɢᴇꜱ ᴛᴏ ᴍᴀɪɴᴛᴀɪɴ ɢʀᴏᴜᴘ ɪɴᴛᴇɢʀɪᴛʏ 
+🚫 ᴇᴅɪᴛᴇᴅ ᴍᴇꜱꜱᴀɢᴇꜱ? I ᴅᴇʟᴇᴛᴇ ᴛʜᴇᴍ ᴛᴏ ᴋᴇᴇᴘ ɪɴᴛᴇɢʀɪᴛʏ.
 
-📣 Nᴏᴛɪғɪᴄᴀᴛɪᴏɴs : ʏᴏᴜ"ʟʟ ɪɴғᴏʀᴍᴇᴅ ᴇᴀᴄʜ ᴛɪᴍᴇ ᴀ ᴍᴇssᴀɢᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ !!
+📣 Nᴏᴛɪғɪᴄᴀᴛɪᴏɴs ᴇᴀᴄʜ ᴛɪᴍᴇ ᴀ ᴍᴇssᴀɢᴇ ɪs ᴅᴇʟᴇᴛᴇᴅ!
 
-🦄 𝗚ᴇᴛ Sᴛᴀʀᴛᴇᴅ :
+🦄 𝗛ᴏᴡ ᴛᴏ 𝗦ᴛᴀʀᴛ:
 1. Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ.
-2. I'ʟʟ sᴛᴀʀᴛ ᴘʀᴏᴛᴇᴄᴛɪɴɢ ɪɴsᴛᴀɴᴛʟʏ.
+2. I'ʟʟ ᴘʀᴏᴛᴇᴄᴛ ɪᴛ ɪɴsᴛᴀɴᴛʟʏ!
 
-🌋 Cʟɪᴄᴋ ᴏɴ 𝗔ᴅᴅ 𝗠ᴇ 𝗧ᴏ 𝗬ᴏᴜʀ 𝗚ʀᴏᴜᴘ ᴛᴏ ᴀᴅᴅ ᴍᴇ ᴀɴᴅ ᴋᴇᴇᴘ ᴏᴜʀ ɢʀᴏᴜᴘ sᴀғᴇ!
+🌋 Cʟɪᴄᴋ *𝗔ᴅᴅ 𝗠ᴇ 𝗧ᴏ 𝗬ᴏᴜʀ 𝗚ʀᴏᴜᴘ* ᴛᴏ ᴋᴇᴇᴘ ɪᴛ sᴀғᴇ.
+
+╭━━━⪼ 𝗙ᴏʀ ᴍᴏʀᴇ ʙᴏᴛꜱ, ᴄʜᴇᴄᴋᴏᴜᴛ @BillaSpace!
 """
-    
+
 buttons = [
     [
         InlineKeyboardButton(
             text="𝗔ᴅᴅ 𝗠ᴇ 𝗧ᴏ 𝗬ᴏᴜʀ 𝗚ʀᴏᴜᴘ",
-            url=f"https://t.me/BillaGuardianBot?startgroup=true",
+            url="https://t.me/BillaGuardianBot?startgroup=true",
         ),
     ],
     [
-        InlineKeyboardButton(text="𝗦ᴜᴘᴘᴏʀᴛ", url=f"https://t.me/BillaNothing"),
-        InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/BillaSpace"),
+        InlineKeyboardButton(text="𝗦ᴜᴘᴘᴏʀᴛ", url="https://t.me/BillaNothing"),
+        InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ", url="https://t.me/BillaSpace"),
     ],    
     [
-        InlineKeyboardButton(text="𝗢ᴡɴᴇʀ", url=f"https://t.me/x_ifeelram"),
+        InlineKeyboardButton(text="𝗢ᴡɴᴇʀ", url="https://t.me/x_ifeelram"),
     ],
-    
 ]
 
 IMG = [
-"https://telegra.ph/file/73c9aa7b5e1a2e053d915.jpg",
-"https://telegra.ph/file/6cf4d7a5d07cdbc5c4c4f.jpg",
-"https://telegra.ph/file/3938993e7f83b9201d961.jpg",
-"https://telegra.ph/file/867bd553810ac3a4cf09f.jpg",
-"https://telegra.ph/file/d102719ef028b224e0842.jpg",
-"https://telegra.ph/file/63dbc9108dca4a91121af.jpg",
-"https://telegra.ph/file/5225ee47a9cbb9a0e85b1.jpg",
-"https://telegra.ph/file/ee9751a286fd983f08086.jpg",
-"https://telegra.ph/file/fbfa4262e467652e75d83.jpg",
-"https://telegra.ph/file/865ce3676d535ec83dce9.jpg",
+    "https://telegra.ph/file/73c9aa7b5e1a2e053d915.jpg",
+    "https://telegra.ph/file/6cf4d7a5d07cdbc5c4c4f.jpg",
+    "https://telegra.ph/file/3938993e7f83b9201d961.jpg",
+    "https://telegra.ph/file/867bd553810ac3a4cf09f.jpg",
+    "https://telegra.ph/file/d102719ef028b224e0842.jpg",
+    "https://telegra.ph/file/63dbc9108dca4a91121af.jpg",
+    "https://telegra.ph/file/5225ee47a9cbb9a0e85b1.jpg",
+    "https://telegra.ph/file/ee9751a286fd983f08086.jpg",
+    "https://telegra.ph/file/fbfa4262e467652e75d83.jpg",
+    "https://telegra.ph/file/865ce3676d535ec83dce9.jpg",
 ]
+
 PM_START_IMG = "https://graph.org/file/2ec1070047bce6578fa26-32de46881e8c48d5bf.jpg"
